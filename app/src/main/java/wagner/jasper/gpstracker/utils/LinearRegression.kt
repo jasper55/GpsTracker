4d46245317f6fe20860e4fcbcbf215d6a2b6fe71
Jasper Wagner <jasper55@users.noreply.github.com>
2020-01-12 12:25:49 +0100

    Create LinearRegression.kt

---
+++ app/src/main/java/wagner/jasper/gpstracker/utils/LinearRegression.kt
@@ -0,0 +1,64 @@
+package wagner.jasper.gpstracker.utils
+
+import jdk.nashorn.internal.objects.NativeArray.reduce
+import java.util.Arrays.asList
+
+
+
+object LinearRegression {
+
+    fun predictNext(): Double{
+        return 0.0
+    }
+
+
+    private val x = asList(2, 3, 5, 7, 9, 11, 14) // Consecutive hours developer codes
+    private val y = asList(4, 5, 7, 10, 15, 20, 40) // Number of bugs produced
+
+    private fun predictForValue(predictForDependentVariable: Int): Double {
+        check(x.size == y.size) { "Must have equal X and Y data points" }
+
+        val numberOfDataValues = x.size
+
+        val xSquared = x
+            .stream()
+            .map<Any> { position -> Math.pow(position!!.toDouble(), 2.0) }
+            .collect(Collectors.toList())
+
+        val xMultipliedByY = IntStream.range(0, numberOfDataValues)
+            .map({ i -> x[i.toInt()] * y[i.toInt()] })
+            .boxed()
+            .collect(Collectors.toList())
+
+        val xSummed = x
+            .stream()
+            .reduce { prev, next -> prev!! + next!! }
+            .get()
+
+        val ySummed = y
+            .stream()
+            .reduce { prev, next -> prev!! + next!! }
+            .get()
+
+        val sumOfXSquared = xSquared
+            .stream()
+            .reduce({ prev, next -> prev!! + next!! })
+            .get()
+
+        val sumOfXMultipliedByY = xMultipliedByY
+            .stream()
+            .reduce({ prev, next -> prev!! + next!! })
+            .get()
+
+        val slopeNominator = numberOfDataValues * sumOfXMultipliedByY - ySummed * xSummed
+        val slopeDenominator =
+            numberOfDataValues * sumOfXSquared - Math.pow(xSummed.toDouble(), 2.0)
+        val slope = slopeNominator / slopeDenominator!!
+
+        val interceptNominator = ySummed - slope!! * xSummed
+        val interceptDenominator = numberOfDataValues.toDouble()
+        val intercept = interceptNominator / interceptDenominator
+
+        return slope!! * predictForDependentVariable + intercept!!
+    }
+}
